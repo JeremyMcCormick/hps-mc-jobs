0dbec5d4dd23b95ffc884b545fa41598f5c1f778
Jeremy McCormick <jermccormick@gmail.com>
2020-07-24 20:18:02 -0400

    Use new template format.

--- norman_ecal_fee/jlab/mkjobs.sh
+++ norman_ecal_fee/jlab/mkjobs.sh
@@ -1,2 +1,4 @@
 # Create 8 jobs starting from job ID 1 with padding of 4 chars, reading 1 event per job 
-hps-mc-jobstore -j 1 -r 1100 -p 4 -g events /work/hallb/hps/ngraf/physrun2019/samplingFractionMC/sim/\\*/\\*.slcio 1 -a vars.json job.json.tmpl jobs.json
+#hps-mc-jobstore -j 1 -r 1100 -p 4 -g events /work/hallb/hps/ngraf/physrun2019/samplingFractionMC/sim/\\*/\\*.slcio 1 -a vars.json job.json.tmpl jobs.json
+
+hps-mc-job-template -j 1 -a vars.json -i events $PWD/events.txt 1 ./job.json.tmpl ./jobs.json
